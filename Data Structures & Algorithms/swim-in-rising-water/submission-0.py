1721fea422044ebcadd9bebc8f87f21b9c835079
Nischay Nanaiah <49742321+nischay92@users.noreply.github.com>
2026-07-22 20:51:06 -0400

    Add: swim-in-rising-water - submission-0

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        # djikstras
+        N = len(grid)
+        visit = set()
+        minHeap = [[grid[0][0], 0, 0]]
+        visit.add((0, 0))
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        while minHeap:
+            t, r, c = heapq.heappop(minHeap)
+            if r == N - 1 and c == N - 1:
+                return t
+            for dr, dc in directions:
+                neiR, neiC = r + dr, c + dc
+                if (neiR < 0 or neiC < 0 or neiR == N or neiC == N or 
+                    (neiR, neiC) in visit):
+                    continue
+                visit.add((neiR, neiC))
+                heapq.heappush(minHeap, [max(t, grid[neiR][neiC]), neiR, neiC])
+
+            
